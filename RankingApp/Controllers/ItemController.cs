287d991b69f1e9ab67671845435669531e5346ec
Mark <fqchenxin@gmail.com>
2024-01-02 22:40:54 +1100

    update app

---
+++ RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankingApp.Models;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller")]
+    public class ItemController: ControllerBase
+    {
+        private static readonly IEnumerable<ItemModel> Items = new[]
+        {
+            new ItemModel { Id = 1, Title = "The Godfather", ImageId = 1, Ranking = 0, ItemType = 1},
+        };
+    }
+}
