547696fe512058f5485697964ffc5a76f16220be
Mark <fqchenxin@gmail.com>
2024-01-08 23:58:04 +1100

    add images

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import MovieImageArr from './MovieImage.js';
 
 export const RankItems = () => {
   const [items, setItems] = useState([]);
@@ -16,9 +17,15 @@ export const RankItems = () => {
 
   return (
     <main>
-      {
-        (items !== null) ? items.map((item) => <h3>{item.title}</h3>):<div>Loading...</div>
-      }
+      <div className="items-not-ranked">
+        {
+          (items.length > 0) ? items.map((item) => 
+            <div className="unranked-cell">
+              <img key={`item-${item.id}`} id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image}/>
+            </div>
+          ):<div>Loading...</div>
+        }
+      </div>
     </main>
   )
 }
